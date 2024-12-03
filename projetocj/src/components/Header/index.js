bc175e6054889d5c0cb3626c4137e8cbd73ad62c
João Pedro <joaodesoouza@gmail.com>
2024-12-03 19:40:16 -0400

    Criadas Paginas novas

--- projetocj/src/components/Header/index.js
+++ projetocj/src/components/Header/index.js
@@ -1,29 +1,30 @@
 import { useNavigate, Link } from "react-router-dom";
 import "./header.css";
 
-export const Header = () => {
-    const navigate = useNavigate();
-    
-    const toHome = () => {
-        navigate("/"); // Página inicial
-    };
+// Componente reutilizável para links de navegação
+const NavigationLink = ({ to, className, children }) => (
+  <Link to={to} className={className}>
+    {children}
+  </Link>
+);
 
-    return (
-        <header>
-            <div className="menu">
-                <div className="titulo">
-                    <label htmlFor="cj">Projeto CJ</label>
-                    <a className="nomeProjeto" onClick={toHome}>Projeto CJ</a>
-                </div>
-            </div>
-            <div>
-                <input className="pesquisa" type="text" placeholder="Pesquisar" />
-            </div>
-            <div className="login">
-                <label>
-                    <Link to="/Login" className="linkLogin"> Login </Link>
-                </label>
-            </div>
-        </header>
-    )
+export const Header = () => {
+  return (
+    <header>
+      <div className="menu">
+        <div className="titulo">
+          <Link to="/" className="nomeProjeto">Projeto CJ</Link>
+        </div>
+      </div>
+      <div>
+        <input className="pesquisa" type="text" placeholder="Pesquisar" />
+      </div>
+      <nav className="navigation">
+        <NavigationLink to="/Login" className="linkLogin">Login</NavigationLink>
+        <NavigationLink to="/Ganhadores" className="linkGanhadores">Ganhadores</NavigationLink>
+        <NavigationLink to="/MeusSorteios" className="linkMeusSorteio">Meus Sorteios</NavigationLink>
+        <NavigationLink to="/Produtos" className="linkProdutos">Produtos</NavigationLink>
+      </nav>
+    </header>
+  );
 };
