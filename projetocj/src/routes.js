a7e1f8a9f1a959a5f38119e3c4c211f49beb627e
João Pedro <joaodesoouza@gmail.com>
2024-12-03 19:43:51 -0400

    Routes atulizadas

--- projetocj/src/routes.js
+++ projetocj/src/routes.js
@@ -3,7 +3,11 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Header } from './components/Header';
 import { Footer } from './components/Footer';
 
-import { Home } from './pages/Home'; // Alterado para importação nomeada
+import { Home } from './pages/Home';
+import { Login } from './pages/Login';
+import { Ganhadores } from './pages/Ganhadores';
+import { MeusSorteios } from './pages/MeusSorteios';
+import { Produtos } from './pages/Produtos';
 
 function RoutesApp () {
   return (
@@ -11,6 +15,10 @@ function RoutesApp () {
       <Routes>
         <Route>
             <Route path="/" element={<Home />} />
+            <Route path="/Login" element={<Login />} />
+            <Route path="/Ganhadores" element={<Ganhadores />} />
+            <Route path="/MeusSorteios" element={<MeusSorteios />} />
+            <Route path="/Produtos" element={<Produtos />} />
         </Route>
       </Routes>
     </BrowserRouter>
