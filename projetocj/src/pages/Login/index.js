2694633b7f91a4a6ffd4cba632102d7acc313f88
João Pedro <joaodesoouza@gmail.com>
2025-01-13 10:56:54 -0400

    Feito botão de home

--- projetocj/src/pages/Login/index.js
+++ projetocj/src/pages/Login/index.js
@@ -1,10 +1,24 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
+import "./login.css";
 
 const Login = () => {
+  const navigate = useNavigate();
+
+  const toHome = () => {
+    navigate("/");
+  };
+
   return (
-    <div>
-      <h1>Login Page</h1>
-    </div>
+    <>
+      <div className="Home">
+        <a className="nomeProjeto" onClick={toHome}>Home</a>
+      </div>
+
+      <div>
+        <h1>Login Page</h1>
+      </div>
+    </>
   );
 }
 
